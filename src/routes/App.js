0d87cd6b1637295862ccf0f2eec4dd6611c42b65
eessm01 <eessm01@gmail.com>
2019-10-23 23:52:08 -0500

    card component and product list

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,14 @@
-// import React from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ProductList from '../containers/ProductList';
 
-// const App = () => {
-//   <BrowserRouter />;
-// };
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={ProductList} />
+    </Switch>
+  </BrowserRouter>
+);
 
-// export default App;
+export default App;
+
