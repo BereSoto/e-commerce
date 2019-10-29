05ef9ae083eeefd5dab7cd8c7ff117942dd6bc6c
BereSoto <beresoto.f@gmail.com>
2019-10-28 22:54:10 -0600

    comenzando estilos del modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Modal = ({ showModal }) => {
-  const MODAL = showModal ?
-    <div>Hola mundo</div> : null;
+  const MODAL = showModal ? (
+    <div className='Modal'>
+      <div className='Modal__header'>
+        <p>Crea una cuenta</p>
+      </div>
+
+    </div>
+  ) :
+    null;
 
   return (
     MODAL
