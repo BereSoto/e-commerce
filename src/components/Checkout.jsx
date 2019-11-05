0be99638ba75c07a509a90c51a6dc7d448f478d7
Ingrid Sagrero <starlling@hotmail.com>
2019-11-04 20:44:19 -0600

    creating pay-method view

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Checkout = () => (
   <section className=''>
-    <h1>Pay me!!!</h1>
+    <form className="row">
+      <div className="col-12 col-lg-6 pay-method">
+        <input type="radio" />Credit Card visa
+        <input type="radio" />Credit Card Mastercard
+      </div>
+      <div className="col-12 .col-lg-6">
+        <p>Card number</p>
+        <input type="text"/>
+        <p>Expiration date</p>
+        <select name="month" id="">Month</select>
+        <select name="year" id="">Year</select>
+        <input type="text" /> CVV
+        <input type="Name"/> Full name on a Card
+      </div>
+      <div className="col-12 .col-lg-6">
+        <button>pay</button>
+      </div>
+    </form>
+   
   </section>
 );
 
