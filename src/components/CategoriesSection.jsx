7bdc6239b8d05e5d85f2ffbe4b68c566e6168b36
BereSoto <beresoto.f@gmail.com>
2019-11-05 00:17:18 -0600

    hover cards home

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -2,14 +2,59 @@ import React from 'react';
 import curva from '../assets/static/ondita.svg';
 
 const CategoriesSection = () => (
-  <div className='categories'>
-    <h1>otra seccion</h1>
-    <img
-      src={curva}
-      className='curva'
-      alt=''
-    />
-  </div>
+  <section className='categories-home'>
+    <div className='categories-home__content'>
+      <h2>Te recomendamos</h2>
+      <div className='categories-home__cards'>
+        <div className='categories-home__card'>
+          <figure className='snip0015'>
+            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample38.jpg' alt='sample38' />
+            <figcaption>
+              <h2>
+ropa
+              </h2>
+
+              <a href='#' />
+            </figcaption>
+          </figure>
+        </div>
+        <div className='categories-home__card'>
+          <figure className='snip0015'>
+            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample38.jpg' alt='sample38' />
+            <figcaption>
+              <h2>
+I don't know
+                {' '}
+                <span>which</span>
+                {' '}
+is worse
+              </h2>
+              <p>That everyone has his price, or that the price is always so low.</p>
+              <a href='#' />
+            </figcaption>
+          </figure>
+        </div>
+        <div className='categories-home__card'>
+          <figure className='snip0015'>
+            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample38.jpg' alt='sample38' />
+            <figcaption>
+              <h2>
+I don't know
+                {' '}
+                <span>which</span>
+                {' '}
+is worse
+              </h2>
+              <p>That everyone has his price, or that the price is always so low.</p>
+              <a href='#' />
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+
+    </div>
+
+  </section>
 
 );
 
