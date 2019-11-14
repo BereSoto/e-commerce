ccdbe03bf9b534315e0a4f98a2bb05baa430966f
BereSoto <beresoto.f@gmail.com>
2019-11-13 19:36:27 -0600

    creating the store

---
+++ src/components/Stores.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Stores = () => (
+  <section>
+    <h1>soy una tienda</h1>
+  </section>
+);
+
+export default Stores;
--- src/routes/App.js
+++ src/routes/App.js
@@ -8,6 +8,7 @@ import Wishes from '../components/Wishes';
 import Checkout from '../components/Checkout';
 import NotFound from '../containers/NotFound';
 import UserProfile from '../components/UserProfile';
+import Stores from '../components/Stores';
 
 const App = () => (
   <>
@@ -20,6 +21,7 @@ const App = () => (
         <Route exact path='/wishes' component={Wishes} />
         <Route exact path='/checkout' component={Checkout} />
         <Route exact path='/userprofile' component={UserProfile} />
+        <Route exact path='/stores' component={Stores} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
