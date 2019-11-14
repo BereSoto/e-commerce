8cd8566c90d4ab4379001c8b854a9c02afabd320
BereSoto <beresoto.f@gmail.com>
2019-11-14 00:36:38 -0600

    icons footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-//import instagramIcon from '../assets/static/instagram-logo.svg'
-//import FacebookIcon from '../assets/static/facebook-logo.svg'
+import instagramIcon from '../assets/static/instagram-logo.svg';
+import FacebookIcon from '../assets/static/facebook-logo.svg';
 import heartIcon from '../assets/static/heart-icon.png';
 
 const Footer = () => (
   <footer className='footer row'>
 
     <div className='footer__social col-2'>
-      <img className='footer__img' src={heartIcon} alt='logoprueba' />
-      <img className='footer__img' src={heartIcon} alt='logoprueba' />
+      <img className='footer__img' src={instagramIcon} alt='instagram' />
+      <img className='footer__img' src={FacebookIcon} alt='faceebok' />
     </div>
     <div className=' footer__rigths col-4'>
       <p>Todos los derechos reservados &copy; 2019</p>
