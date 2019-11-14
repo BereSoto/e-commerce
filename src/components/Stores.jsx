9bd4ff8b66b69d8cd1caaa58f03f3fd4d4a8fc5f
BereSoto <beresoto.f@gmail.com>
2019-11-14 00:34:53 -0600

    styles store

--- src/components/Stores.jsx
+++ src/components/Stores.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import userPurple from '../assets/static/user-purple.svg';
+import facebook from '../assets/static/facebook-logo.svg';
+import instagram from '../assets/static/instagram-logo.svg';
 
 const Stores = () => (
   <section>
@@ -7,14 +9,26 @@ const Stores = () => (
       <div className='stores-column__left'>
         <img className='user-columns__profile' src={userPurple} alt='logouser' />
         <div className='column-social'>
-          <p>Adriana</p>
-          <p>Sánchez</p>
+          <h2 className='column-social__title'>Adriana Sánchez </h2>
+          <h3>Hobbies</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae voluptatum sed aut.</p>
+          <div className='social-networks'>
+            <img src={facebook} className='social-networks__icons' alt='' />
+            <img src={instagram} className='social-networks__icons' alt='' />
+          </div>
+        </div>
+      </div>
+      <div className='stores-column__rigth'>
+        <div className='stores-column__trademark'>
+          <h2>Maizajo</h2>
+          <div>
+            aqui van las cards de productos
+          </div>
+          <h3>Sobre nosotros</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati vero exercitationem, rerum ut dolore necessitatibus quae autem odit tempore delectus quisquam labore? Blanditiis, cum enim quis error autem eum sapiente!</p>
 
         </div>
 
-      </div>
-      <div className='stores-column__rigth'>
-        <h1>hola</h1>
       </div>
 
     </div>
