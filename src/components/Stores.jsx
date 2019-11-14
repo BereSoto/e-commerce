f413c874c26b654e8c6cd7f6b696f1f1cdbe6cb0
BereSoto <beresoto.f@gmail.com>
2019-11-13 23:16:01 -0600

    store styles

--- src/components/Stores.jsx
+++ src/components/Stores.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import userPurple from '../assets/static/user-purple.svg';
 
 const Stores = () => (
   <section>
-    <h1>soy una tienda</h1>
+    <div className='stores-column'>
+      <div className='stores-column__left'>
+        <img className='user-columns__profile' src={userPurple} alt='logouser' />
+        <div className='column-social'>
+          <p>Adriana</p>
+          <p>Sánchez</p>
+
+        </div>
+
+      </div>
+      <div className='stores-column__rigth'>
+        <h1>hola</h1>
+      </div>
+
+    </div>
   </section>
 );
 
