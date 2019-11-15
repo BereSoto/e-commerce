d8c876b0da1c8a107781f5c663f809f1533355f5
BereSoto <beresoto.f@gmail.com>
2019-11-15 13:37:26 -0600

    creating social media button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FacebookIcon from '../assets/static/facebook-logo.svg';
 
 const Modal = ({ showModal }) => {
   const MODAL = showModal ? (
@@ -6,21 +7,32 @@ const Modal = ({ showModal }) => {
       <div className='modal__container'>
         <div className='modal__header'>
           <p>INICIA SESIÓN</p>
+          {/* <p>CLOSE</p> */}
         </div>
-        <form action='modal__form'>
-          <label htmlFor='' className='modal__data'>Correo electronico</label>
-          <input type='text' name='' id='' placeholder='correo electronico' />
-          <label htmlFor='' className='modal__data'>Contraseña</label>
-          <input type='text' name='' id='' placeholder='contraseña' />
-          <button className='modal__btn'>Enviar</button>
-        </form>
-        <div>
-          <p>O inicia con</p>
-          <button>Facebook</button>
+        <div className=''>
+          <form className='modal__form'>
+            <label className='modal__data'>
+              Correo electronico
+              <input type='text' name='name' />
+            </label>
+            <label className='modal__data'>
+              Contraseña
+              <input type='email' name='name' />
+            </label>
+            <input type='submit' value='Enviar' className='form__btn' />
+          </form>
         </div>
-
+        <div className='modal__social'>
+          <p>O</p>
+          <button type='submit' className='modal__fb' id=''>
+            <img className='footer__img' src={FacebookIcon} alt='faceebok' />
+          Facebook
+          </button>
+          <p>
+            <a href=''>Crea una cuenta</a>
+          </p>
+        </div>
       </div>
-
     </div>
 
   ) :
