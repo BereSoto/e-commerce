ed8821d09edc852444d58fe428c47919c291b3c5
BereSoto <beresoto.f@gmail.com>
2019-11-18 19:35:28 -0600

    quitando comentario

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 //const respuesta = await axios.get('http://e-moms-api.now.sh/api/products');
-export const fetchProducts = () => dispatch => {
-  const URL_API = 'http://e-moms-api.now.sh/api/products'
-  // const PROXY_URL = "https://obscure-citadel-86298.herokuapp.com/"
-  axios.get(`${URL_API}`).then(res => {
+export const fetchProducts = () => (dispatch) => {
+  const URL_API = 'http://e-moms-api.now.sh/api/products';
+  const PROXY_URL = 'https://obscure-citadel-86298.herokuapp.com/';
+  axios.get(`${PROXY_URL}${URL_API}`).then((res) => {
     dispatch({
-      type: "FETCH_PRODUCTS",
-      payload: res.data.data
+      type: 'FETCH_PRODUCTS',
+      payload: res.data.data,
     });
   });
 };
 
 //Agregar al carrito
-export const addToCart = payload => ({
+export const addToCart = (payload) => ({
   type: 'ADD_TO_CART',
   payload,
 });
 
-export const deleteProduct = payload => ({
+export const deleteProduct = (payload) => ({
   type: 'DELETE_PRODUCT',
   payload,
 });
