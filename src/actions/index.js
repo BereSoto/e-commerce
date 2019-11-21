9a8ccb0c3193a505fb6db78f2d9154c09d0641c4
Montse Soto <32855760+BereSoto@users.noreply.github.com>
2019-11-20 22:59:25 -0600

    Variables en archivo único

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
+import { PROXY_URL, URL_API } from '../constants';
 //const respuesta = await axios.get('http://e-moms-api.now.sh/api/products');
 export const fetchProducts = () => (dispatch) => {
-  const URL_API = 'http://e-moms-api.now.sh/api/products';
-  const PROXY_URL = 'https://obscure-citadel-86298.herokuapp.com/';
   axios.get(`${PROXY_URL}${URL_API}`).then((res) => {
     dispatch({
       type: 'FETCH_PRODUCTS',
